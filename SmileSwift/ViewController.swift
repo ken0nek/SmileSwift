ceca015d7a94f1ad74e0adf1a883e5959af9f0eb
ken0nek <ken.for.develop@gmail.com>
2018-01-14 14:49:30 +0900

    Refactor

--- SmileSwift/ViewController.swift
+++ SmileSwift/ViewController.swift
@@ -9,13 +9,12 @@
 import UIKit
 
 final class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
+
     @IBOutlet private weak var outputTextView: UITextView!
     @IBOutlet private weak var sampleImageView: UIImageView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
         detectFaces()
     }
@@ -48,38 +47,48 @@ final class ViewController: UIViewController, UIImagePickerControllerDelegate, U
 
     // MARK: -
     private func detectFaces() {
+
+        // create CGImage from image on storyboard.
+        guard let image = sampleImageView.image,
+            let cgImage = image.cgImage else {
+            return
+        }
+
         DispatchQueue.global(qos: .background).async {
             
-            // create CGImage from image on storyboard.
-            guard let image = self.sampleImageView.image, let cgImage = image.cgImage else {
-                return
-            }
-            
             let ciImage = CIImage(cgImage: cgImage)
-            
+
             // set CIDetectorTypeFace.
             let detector = CIDetector(ofType: CIDetectorTypeFace, context: nil, options: [CIDetectorAccuracy: CIDetectorAccuracyHigh])
             
-            // set options
-            let options = [CIDetectorSmile : true, CIDetectorEyeBlink : true]
-            
-            // get features from image
-            let features = detector?.features(in: ciImage, options: options)
-            
-            var resultString = "DETECTED FACES:\n\n"
+            // set detector options
+            let options = [
+                CIDetectorSmile: true,
+                CIDetectorEyeBlink: true
+            ]
 
-            for feature in features as! [CIFaceFeature] {
-                resultString.append("bounds: \(NSStringFromCGRect(feature.bounds))\n")
-                resultString.append("hasSmile: \(feature.hasSmile ? "YES" : "NO")\n")
-                resultString.append("faceAngle: \(feature.hasFaceAngle ? String(feature.faceAngle) : "NONE")\n")
-                resultString.append("leftEyeClosed: \(feature.leftEyeClosed ? "YES" : "NO")\n")
-                resultString.append("rightEyeClosed: \(feature.rightEyeClosed ? "YES" : "NO")\n")
-                
-                resultString.append("\n")
-            }
-            
+            // get face features from image
+            let features = detector?.features(in: ciImage, options: options) as! [CIFaceFeature]
+
+            let featuresDescription = features
+                .map { f in
+                    // you can configure CIFaceFeature descriptions here. There are many other properties you can get.
+                    return """
+                    **********
+                    bounds: \(f.bounds)
+                    hasSmile: \(f.hasSmile)
+                    hasfaceAngle: \(f.hasFaceAngle) -> \(f.faceAngle)
+                    leftEyeClosed: \(f.leftEyeClosed)
+                    rightEyeClosed: \(f.rightEyeClosed)
+                    **********
+                    """
+                }
+                .joined(separator: "\n")
+
+            let outputText = "DETECTED FACES (\(features.count)):\n\n" + featuresDescription
+
             DispatchQueue.main.async { () -> Void in
-                self.outputTextView.text = "\(resultString)"
+                self.outputTextView.text = "\(outputText)"
             }
         }
     }
